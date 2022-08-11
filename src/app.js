176b76bfa9bb78281c6b1129e8b2b486995b2de9
Natalya <natashanatalia883@gmail.com>
2022-08-11 10:43:43 +0300

    Added the formatDate

--- src/app.js
+++ src/app.js
@@ -1,17 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minuters = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let tempElement = document.querySelector(".temperature-number");
   let cityElement = document.querySelector("#header-city");
   let countryElement = document.querySelector("#header-country");
+  let dateElement = document.querySelector("#header-date");
   let descriptionElement = document.querySelector("#header-description");
-  let presipitationElement = document.querySelector("#presipitation");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+
   tempElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   countryElement.innerHTML = response.data.sys.country;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   descriptionElement.innerHTML = response.data.weather[0].description;
-  //presipitationElement.innerHTML = response.data.;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
